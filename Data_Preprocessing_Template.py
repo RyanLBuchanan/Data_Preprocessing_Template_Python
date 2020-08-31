3b122e175a96f0f33f98200cedf260f6140eb191
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-31 16:08:41 -0600

    .gitignore & Jupyter notebook

---
+++ Data_Preprocessing_Template.py
@@ -0,0 +1,25 @@
+# Data Prepocessing Template from Machine Learning A-Z - SuperDataScience
+# Input by Ryan L Buchanan 31AUG20
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+
+# Taking care of the missing data
+
+
+# Encoding categorical data
+
+
+# Encoding the Independent Variable
+
+
+# Encoding the Dependent Variable
+
+
+
+
