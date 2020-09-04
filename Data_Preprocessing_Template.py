4e28f85768dadce3a353a1b83c7c8ad4a61e4bb3
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-03 20:03:05 -0600

    Encoded yesses and nos to 1s and 0s

--- Data_Preprocessing_Template.py
+++ Data_Preprocessing_Template.py
@@ -9,7 +9,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1 ].values
+y = dataset.iloc[:, -1].values
 
 print(X)
 print(y)
@@ -22,13 +22,19 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 print(X)
 
 # Encoding categorical data
-
-
 # Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
 
+print(X)
 
 # Encoding the Dependent Variable
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
 
-
+print(y)
 
 
