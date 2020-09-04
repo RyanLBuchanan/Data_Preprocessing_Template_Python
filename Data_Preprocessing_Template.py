524151726c878e649750b47e702a0376df41baf6
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-03 19:17:40 -0600

    Used SimpleImputer to replace nans with averages of their respective columns

--- Data_Preprocessing_Template.py
+++ Data_Preprocessing_Template.py
@@ -9,10 +9,17 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+y = dataset.iloc[:, -1 ].values
+
+print(X)
+print(y)
 
 # Taking care of the missing data
-
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
 
 # Encoding categorical data
 
